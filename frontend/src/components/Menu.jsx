84c1a949ff7f73a631f8a2270d15104a6ddbd6ee
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-08-26 17:24:17 -0300

    Refactor menú: reemplazo de enlaces por componente MenuItem y mejora de estilos

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import MenuItem from './MenuItem';
 
 export default function Menu() {
   return <nav
@@ -6,9 +6,9 @@ export default function Menu() {
       backgroundColor: 'lightblue',
     }}
   >
-    <Link to="/">Inicio</Link>
-    <Link to="/login">Login</Link>
-    <Link to="/about">Acerca de</Link>
-    <Link to="/otra">Otra cosa</Link>
+    <MenuItem to="/">Inicio</MenuItem>
+    <MenuItem to="/login">Login</MenuItem>
+    <MenuItem to="/about">Acerca de</MenuItem>
+    <MenuItem to="/otra">Otra cosa</MenuItem>
   </nav>;
 }
