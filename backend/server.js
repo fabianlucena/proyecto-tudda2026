0c9499992fbd1560c39de01b1efdd242abf27306
Fabian Lucena <85037034+fabianlucena@users.noreply.github.com>
2026-08-26 19:26:53 -0300

    Agregar soporte para CORS en el servidor y actualizar dependencias en package.json

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import config from './config.js';
 import { configureRouter } from './api/router.js';
 import './dependencies.js';
@@ -13,6 +14,7 @@ import logMiddleware from './middlewares/log_middleware.js';
 try {
   const app = express();
 
+  app.use(cors());
   app.use(express.json());
   app.use(logMiddleware);
   app.use(checkAuthorizationTokenMiddleware);
